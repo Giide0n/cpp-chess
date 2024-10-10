f44e93b3c81ed266f62ad60eb95343ee2235c0a5
gideon <gideon.monterosa@gmail.com>
2024-10-11 00:31:45 +0200

    draw a simple chess board

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+std::string board[64] = {"♖", "♘", "♗", "♔", "♕", "♗", "♘", "♖", "♙", "♙", "♙",
+                         "♙", "♙", "♙", "♙", "♙", "·", "·", "·", "·", "·", "·",
+                         "·", "·", "·", "·", "·", "·", "·", "·", "·", "·", "·",
+                         "·", "·", "·", "·", "·", "·", "·", "·", "·", "·", "·",
+                         "·", "·", "·", "·", "♟", "♟", "♟", "♟", "♟", "♟", "♟",
+                         "♟", "♜", "♞", "♝", "♚", "♛", "♝", "♞", "♜"};
+
+void drawBoard() {
+  for (size_t i = 0; i < std::size(board); i++) {
+    if (i % 8 == 0) {
+      std::cout << std::endl << 8 - (i / 8) << ' ';
+    }
+    std::cout << ' ' << board[63 - i] << ' ';
+  }
+  std::cout << std::endl << "  ";
+  for (size_t i = 0; i < 8; i++) {
+    std::cout << ' ' << static_cast<char>('a' + i) << ' ';
+  }
+  std::cout << std::endl;
+}
+
 int main() {
-  std::cout << "Hello World" << std::endl;
-  return 0;
+  std::string input;
+  while (std::cin >> input && input != "/") {
+    std::cout << "You Typed: " << input;
+    drawBoard();
+  }
 }
